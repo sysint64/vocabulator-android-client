cf342725ed34522d3203d4ae7631ca64564333d3
Andrey Kabylin <andrey@kabylin.ru>
2018-08-04 00:18:00 +0700

    clean up

--- app/src/main/java/ru/kabylin/andrey/vocabulator/MainActivity.kt
+++ app/src/main/java/ru/kabylin/andrey/vocabulator/MainActivity.kt
@@ -14,17 +14,12 @@ import ru.kabylin.andrey.vocabulator.client.RequestState
 import android.support.v7.widget.helper.ItemTouchHelper
 import android.view.Menu
 import android.view.MenuItem
-import io.grpc.ManagedChannelBuilder
-import io.grpc.examples.helloworld.GreeterGrpc
 import ru.kabylin.andrey.vocabulator.ext.*
 import ru.kabylin.andrey.vocabulator.holders.CategoryCardHolder
 import ru.kabylin.andrey.vocabulator.services.SyncService
 import ru.kabylin.andrey.vocabulator.tools.isNetworkAvailable
 import ru.kabylin.andrey.vocabulator.views.*
 import java.util.*
-import io.grpc.examples.helloworld.HelloRequest
-import io.reactivex.Single
-import org.jetbrains.anko.alert
 
 class MainActivity : ClientAppCompatActivity<ClientViewState>(), KodeinAware {
     override val kodeinContext = kcontext(this)
